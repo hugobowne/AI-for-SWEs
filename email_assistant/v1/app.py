d0cead811bd7d7b31a8b4b4dfba3a7f45128c7d7
Stefan Krawczyk <stefan@dagworks.io>
2024-11-02 22:33:18 -0700

    Simplifies V1
    This makes the gradio app:
    
    1. a simple form submit
    2. creates a stable session ID to see Burr traces for a PDF in a single place.
    
    Otherwise point of V1 is to play:
    
     - e.g. "write a haiku about this profile"
     - e.g. "extract [name, position, knows python?, highest education level]"
     - e.g. "write a haiku about this profile and extract [name, position, knows python? , highested education level]"

--- email_assistant/v1/app.py
+++ email_assistant/v1/app.py
@@ -2,67 +2,47 @@ import gradio as gr
 from assistant import build_assistant
 
 
-def add_user_message(history: list[dict], message: dict, pdf_file: str) -> tuple:
-    """Add the user message to conversation history, display it, and extract PDF file paths."""
-    pdf_file_path = pdf_file
-
-    for x in message["files"]:
-        # display the file as the user's message
-        history.append({"role": "user", "content": {"path": x}})
-        # add the file to the list of PDFs if is a PDF
-        if x.endswith(".pdf"):
-            pdf_file_path = x
-
-    # display the user's message
-    if message["text"]:
-        history.append({"role": "user", "content": message["text"]})
-
-    # the order of returned components must match the order of `chat_input.submit()`
-    return history, gr.MultimodalTextbox(value=None, interactive=False), gr.Files(value=pdf_file_path)
-
-
-def run_assistant(history: list[dict], pdf_file: str) -> list[dict]:
-    """Run the assistant on the latest user message and current PDF file.
+def run_pipeline(pdf_file: str, session_id: gr.State, query_input: str) -> list[dict]:
+    """Run the pipeline on the latest user message and current PDF file.
     This doesn't leverage the full conversation history.
     """
-    instructions = history[-1]["content"]  # retrieve the last user message
-    # TODO handle when last message was a file; `content` will be a dict instead of str
-
     if pdf_file:
-        assistant = build_assistant()
+        assistant = build_assistant(session_id.value)
         action_name, results, state = assistant.run(
-            halt_after=["generate_email"],
-            inputs={"pdf_file_path": pdf_file, "instructions": instructions}
+            halt_after=["generate_response"],
+            inputs={"pdf_file_path": pdf_file, "instructions": query_input}
         )
 
-        history.append({"role": "assistant", "content": state["llm_reply"]})
+        history = [{"role": "assistant", "content": state["llm_reply"]}]
     else:
-        history.append({"role": "assistant", "content": "Please upload a PDF file to continue."})
+        history = [{"role": "assistant", "content": "Please upload a PDF file to continue."}]
 
     return history
 
+def create_session_id(pdf_upload) -> gr.State:
+    """Create a session ID for the assistant using the uploaded file name stripping any characters
+    that would interfere with a file path."""
+    file_name = pdf_upload.name.split("/")[-1]
+    name = file_name.replace(" ", "_").replace(".", "_")
+    # trim to UUID length - going from the end
+    if len(name) > 36:
+        name = name[len(name) - 36:]
+    return gr.State(name)
 
 def build_ui() -> gr.Blocks:
     """Return a Gradio UI for the email assistant."""
     with gr.Blocks() as ui:
-        chatbot = gr.Chatbot(label="Email Assistant", type="messages", height=800, elem_id="chatbot")
-        chat_input = gr.MultimodalTextbox(
-            interactive=True,
-            file_count="single",
-            placeholder="Send a message or upload a file",
-            show_label=False,
-        )
-        pdf_files = gr.File(value=None, label="Uploaded PDF", interactive=False)
-
-        # add the user message to history and display it
-        # then run the assistant
-        # then refresh the chat input
-        chat_input.submit(
-            add_user_message, [chatbot, chat_input, pdf_files], [chatbot, chat_input, pdf_files]
+        pdf_upload = gr.File(label="Upload PDF", type="filepath")  # Handle file
+        query_input = gr.Textbox(label="Type prompt instructions here")
+        response_box = gr.Chatbot(label="Response", type="messages", height=400, elem_id="response_box")
+        session_id_value = gr.State(None)  # Store conversation ID
+        query_button = gr.Button("Submit")
+        # create the session id based on the uploaded file name
+        # then run the pipeline and update the response box
+        query_button.click(
+            create_session_id, [pdf_upload], [session_id_value]
         ).then(
-            run_assistant, [chatbot, pdf_files], chatbot
-        ).then(
-            lambda: gr.MultimodalTextbox(interactive=True), None, [chat_input]
+            run_pipeline, [pdf_upload, session_id_value, query_input], response_box
         )
 
     return ui
--- email_assistant/v1/assistant.py
+++ email_assistant/v1/assistant.py
@@ -1,21 +1,34 @@
+from typing import Optional
+
 import openai
 from burr.core import action, ApplicationBuilder, Application, State
-from pypdf import PdfReader
+import pymupdf
+from burr.tracking import LocalTrackingClient
+
+# this will auto instrument the openAI client. No swapping of imports required!
+from opentelemetry.instrumentation.openai import OpenAIInstrumentor
+from opentelemetry.instrumentation.ollama import OllamaInstrumentor
+
+OpenAIInstrumentor().instrument()
+OllamaInstrumentor().instrument()
 
 
 @action(reads=[], writes=["pdf_text"])
 def process_pdf(state: State, pdf_file_path: str) -> State:
     """Extract text from a PDF and return it as a string."""
-    reader = PdfReader(pdf_file_path)
-    text = " ".join([page.extract_text() for page in reader.pages])
+    pdf_doc = pymupdf.open(pdf_file_path, filetype="pdf")
+    text = ""
+    for page_num in range(pdf_doc.page_count):
+        page = pdf_doc.load_page(page_num)
+        text += page.get_text("text")
     return state.update(pdf_text=text)
 
 
 @action(reads=["pdf_text"], writes=["llm_reply"])
-def generate_email(state: State, instructions: str) -> State:
+def generate_response(state: State, instructions: str) -> State:
     """Generate answer based on the PDF's text using an LLM following the instructions"""
     text = state["pdf_text"]
-    client = openai.OpenAI()
+    client = openai.OpenAI() # replace this with ollama as needed.
 
     response = client.chat.completions.create(
         model="gpt-4o-mini",
@@ -28,16 +41,23 @@ def generate_email(state: State, instructions: str) -> State:
     return state.update(llm_reply=response.choices[0].message.content)
 
 
-def build_assistant() -> Application:
+def build_assistant(burr_app_id: Optional[str]) -> Application:
+    tracker = LocalTrackingClient(project="ai-for-swes-email-v1")
     return (
         ApplicationBuilder()
-        .with_actions(process_pdf, generate_email)
-        .with_transitions(("process_pdf", "generate_email"))
-        .with_entrypoint("process_pdf")
+        .with_actions(process_pdf, generate_response)
+        .with_transitions(("process_pdf", "generate_response"))
+        .initialize_from(
+            tracker,
+            resume_at_next_action=False,
+            default_state={},
+            default_entrypoint="process_pdf",
+        ).with_identifiers(app_id=burr_app_id)
+        .with_tracker(tracker, use_otel_tracing=True)  # tracking + checkpointing; one line 🪄.
         .build()
     )
 
 
 if __name__ == "__main__":
-    app = build_assistant()
+    app = build_assistant(None)
     app.visualize("assistant_v1.png", include_state=True)
